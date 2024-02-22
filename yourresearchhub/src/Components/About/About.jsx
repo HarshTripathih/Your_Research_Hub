5f3ab0c08546e6d6b6c709af39b5f146303a60ac
BIPUL KUMAR <94435979+bipulkumarb@users.noreply.github.com>
2024-02-23 00:54:17 +0530

    updatedaboutpage

--- yourresearchhub/src/Components/About/About.jsx
+++ yourresearchhub/src/Components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-col text-center w-full mb-20">
             <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">OUR TEAM</h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">With over 10 years of combined experience, we've got a well seasoned team at the helm</p>
           </div>
           <div class="flex flex-wrap -m-4">
             <div class="p-4 lg:w-1/2">
